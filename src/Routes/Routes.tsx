e233e825b134d95cb9b8b8e2434d148874bd7a96
Nazmul Hasan Nannu <nazmulnannu43@gmail.com>
2023-03-18 23:03:02 +0600

    dashboard added

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../Layout/Dashboard/Dashboard/Dashboard";
 import Main from "../Layout/Main/Main";
 import Error from "../Pages/Error";
 import Home from "../Pages/Home";
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
   },
 ]);
 
