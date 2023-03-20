ca0e0d95b1498753028e82b08e62ab4cea322a4b
Nazmul Hasan Nannu <nazmulnannu43@gmail.com>
2023-03-20 22:29:47 +0600

    add some code in dashboard

--- src/Layout/Dashboard/Dashboard/Dashboard.tsx
+++ src/Layout/Dashboard/Dashboard/Dashboard.tsx
@@ -2,8 +2,37 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>This Is Dashboard Page</h2>
+    <div className="bg-white">
+      <main>
+        <div className="wrapper flex">
+          <div className="siedbar w-72 bg-red-500 p-5">
+            <ul className="flex flex-col">
+              <li>ome</li>
+              <li>gd</li>
+              <li>List</li>
+              <li>List2</li>
+              <li>List3</li>
+              <li>List4</li>
+              <li>List5</li>
+              <li>List6</li>
+              <li>List7</li>
+              <li>List8</li>
+              <li>This complex layout</li>
+              <li>Thats great Layout</li>
+              <li>Lets See what actually happend</li>
+              <li>Lets See</li>
+            </ul>
+          </div>
+          <div className="content">
+            <div className="container">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+              obcaecati ipsum reiciendis ea omnis eius deserunt velit, aperiam
+              consectetur illo totam tenetur repudiandae fuga numquam asperiores
+              ullam adipisci sunt corrupti.
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
