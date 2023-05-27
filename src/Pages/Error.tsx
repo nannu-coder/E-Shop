11324a64be01d267c569e52c576252f785d58181
nannu-coder <nazmulnannu43@gmail.com>
2023-05-27 22:16:37 +0600

    add text

--- src/Pages/Error.tsx
+++ src/Pages/Error.tsx
@@ -6,7 +6,7 @@ const Error = () => {
       <h2>404 NOT FOUND</h2>
       <h5>Make Sure You Are Right URL</h5>
       <h1>Error Page</h1>
-      <h1>Go Back</h1>
+      <h1>Go to Back</h1>
     </div>
   );
 };
